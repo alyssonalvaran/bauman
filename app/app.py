2b53a93853ce645886714c70d090530131730e5b
alyssonalvaran <alvaran.alysson@gmail.com>
2018-02-08 11:19:31 +0800

    Add initial files
    - Add layout page
    - Add navigation bar to layout
    - Add footer to layout
    
    - Add home page
    - Add signup page
    - Add login page
    - Add cart page
    
    - Add jQuery
    - Add Bootstrap
    - Add Python Flask default files to .gitignore

---
+++ app/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+ 
+@app.route("/")
+def index():
+	try:
+		return render_template('index.html')
+	except TemplateNotFound:
+		abort(404)
+
+@app.route("/signup")
+def signup():
+	try:
+		return render_template('signup.html')
+	except TemplateNotFound:
+		abort(404)
+
+@app.route("/login")
+def login():
+	try:
+		return render_template('login.html')
+	except TemplateNotFound:
+		abort(404)
+
+@app.route("/cart")
+def cart():
+	try:
+		return render_template('cart.html')
+	except TemplateNotFound:
+		abort(404)
